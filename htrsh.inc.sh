6025613c3e4a79ecb873e0a8c1b94ddcff1c862a
Mauricio Villegas <mauricio_ville@yahoo.com>
2015-04-22 10:19:18 +0000

    added xmlpage_resize to htrsh

--- htrsh.inc.sh
+++ htrsh.inc.sh
@@ -70,7 +70,7 @@ htrsh_version () {
 htrsh_check_req () {
   local FN="htrsh_check_req";
   local cmd;
-  for cmd in xmlstarlet convert octave HVite pfl2htk imgtxtenh imglineclean imgpageborder imgccomp xmlpage_resize.sh imageSlant realpath gzip page_format_generate_contour pca; do
+  for cmd in xmlstarlet convert octave HVite pfl2htk imgtxtenh imglineclean imgpageborder imgccomp imageSlant realpath gzip page_format_generate_contour pca; do
     local c=$(which $cmd);
     if ! [ -e "$c" ]; then
       echo "$FN: WARNING: unable to find command: $cmd" 1>&2;
@@ -290,9 +290,104 @@ htrsh_pageimg_resize () {
   convert "$IMFILE" -units PixelsPerCentimeter -density $OUTRES -resize $SFACT "$OUTDIR/$IMBASE"; ### don't know why the density has to be set this way
 
   ### Resize XML Page ###
-  cat "$XML" | xmlpage_resize.sh $SFACT > "$OUTDIR/$XMLBASE";
+  cat "$XML" | htrsh_xmlpage_resize $SFACT > "$OUTDIR/$XMLBASE";
 
   return 0;
+}
+
+##
+## Function that resizes a XML Page file
+##
+htrsh_xmlpage_resize () {
+  local FN="htrsh_xmlpage_resize";
+  local newWidth newHeight scaleFact;
+  if [ $# -lt 1 ]; then
+    { echo "$FN: error: not enough input arguments";
+      echo "Usage: $FN ( {newWidth}x{newHeight} | {scaleFact}% ) < XML_PAGE_FILE";
+    } 1>&2;
+    return 1;
+  elif [ $(echo "$1" | grep -P '^[0-9]+x[0-9]+$' | wc -l) = 1 ]; then
+    newWidth=$(echo "$1" | sed 's|x.*||');
+    newHeight=$(echo "$1" | sed 's|.*x||');
+  elif [ $(echo "$1" | grep -P '^[0-9.]+%$' | wc -l) = 1 ]; then
+    scaleFact=$(echo "$1" | sed 's|%$||');
+  fi
+
+  local XSLT='<?xml version="1.0"?>
+<xsl:stylesheet
+  xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xmlns:str="http://exslt.org/strings"
+  xmlns="http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15"
+  xmlns:_="http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15"
+  xmlns:DEFAULT="http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15"
+  extension-element-prefixes="str"
+  version="1.0">
+
+  <xsl:output method="xml" indent="yes" encoding="utf-8" omit-xml-declaration="no"/>
+
+  <xsl:variable name="oldWidth" select="//_:Page/@imageWidth"/>
+  <xsl:variable name="oldHeight" select="//_:Page/@imageHeight"/>';
+
+  if [ "$scaleFact" != "" ]; then
+    XSLT="$XSLT"'
+  <xsl:variable name="scaleWidth" select="number('${scaleFact}') div 100"/>
+  <xsl:variable name="scaleHeight" select="$scaleWidth"/>
+  <xsl:variable name="newWidth" select="round($oldWidth*$scaleWidth)"/>
+  <xsl:variable name="newHeight" select="round($oldHeight*$scaleHeight)"/>';
+  else
+    XSLT="$XSLT"'
+  <xsl:variable name="newWidth" select="'${newWidth}'"/>
+  <xsl:variable name="newHeight" select="'${newHeight}'"/>
+  <xsl:variable name="scaleWidth" select="number($newWidth) div number($oldWidth)"/>
+  <xsl:variable name="scaleHeight" select="number($newHeight) div number($oldHeight)"/>';
+  fi
+
+  XSLT="$XSLT"'
+  <xsl:template match="@* | node()">
+    <xsl:copy>
+      <xsl:apply-templates select="@* | node()"/>
+    </xsl:copy>
+  </xsl:template>
+
+  <xsl:template match="//_:Page">
+    <xsl:copy>
+      <xsl:attribute name="imageWidth">
+        <xsl:value-of select="$newWidth"/>
+      </xsl:attribute>
+      <xsl:attribute name="imageHeight">
+        <xsl:value-of select="$newHeight"/>
+      </xsl:attribute>
+      <xsl:apply-templates select="@*[local-name() != '"'imageWidth'"' and local-name() != '"'imageHeight'"'] | node()" />
+    </xsl:copy>
+  </xsl:template>
+
+  <xsl:template match="//*[@points]">
+    <xsl:copy>
+      <xsl:attribute name="points">
+        <xsl:for-each select="str:tokenize(@points,'"', '"')">
+          <xsl:choose>
+            <xsl:when test="position() = 1">
+              <xsl:value-of select="round(number($scaleWidth)*number(.))"/>
+            </xsl:when>
+            <xsl:when test="position() mod 2 = 0">
+              <xsl:text>,</xsl:text><xsl:value-of select="round(number($scaleHeight)*number(.))"/>
+            </xsl:when>
+            <xsl:otherwise>
+              <xsl:text> </xsl:text><xsl:value-of select="round(number($scaleWidth)*number(.))"/>
+            </xsl:otherwise>
+          </xsl:choose> 
+        </xsl:for-each>
+      </xsl:attribute>
+      <xsl:apply-templates select="@*[local-name() != '"'points'"'] | node()" />
+    </xsl:copy>
+  </xsl:template>
+
+</xsl:stylesheet>';
+
+  cat /dev/stdin | xmlstarlet tr <( echo "$XSLT" );
+
+  return $?;
 }
 
 
