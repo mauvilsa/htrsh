eff360bd6512c454482bdde68874eef83533b6ae
Mauricio Villegas <mauricio_ville@yahoo.com>
2016-07-08 05:45:35 +0000

    - Moved code for preparing tasas file to the main library

--- htrsh_exp.inc.sh
+++ htrsh_exp.inc.sh
@@ -560,29 +560,29 @@ htrsh_exp_htr_cv () {(
       TS=$(($(date +%s%N)/1000000));
       echo "# WER CER WER_canonic CER_canonic WER_diplom CER_diplom" > "$DDIR.txt";
       for f in $(ls "$DDIR"/*.mlf.gz | grep -v 'part[0-9]*.mlf.gz'); do
-        htrsh_mlf_to_tasas \
+        htrsh_prep_tasas \
             "$EXPDIR/groundtruth/$DATASET/pages_test.mlf" \
             <( gzip -dc "$f" ) \
           > "$DDIR/evaluate_${wg}_wer.txt";
-        htrsh_mlf_to_tasas \
+        htrsh_prep_tasas \
             "$EXPDIR/groundtruth/$DATASET/pages_test.mlf" \
             <( gzip -dc "$f" ) \
             -c yes \
           > "$DDIR/evaluate_${wg}_cer.txt";
-        htrsh_mlf_to_tasas \
+        htrsh_prep_tasas \
             <( "$htrsh_canonizer" < "$EXPDIR/groundtruth/$DATASET/pages_test.mlf" ) \
             <( gzip -dc "$f" | "$htrsh_canonizer" ) \
           > "$DDIR/evaluate_${wg}_canonic_wer.txt";
-        htrsh_mlf_to_tasas \
+        htrsh_prep_tasas \
             <( "$htrsh_canonizer" < "$EXPDIR/groundtruth/$DATASET/pages_test.mlf" ) \
             <( gzip -dc "$f" | "$htrsh_canonizer" ) \
             -c yes \
           > "$DDIR/evaluate_${wg}_canonic_cer.txt";
-        htrsh_mlf_to_tasas \
+        htrsh_prep_tasas \
             <( "$htrsh_diplomatizer" < "$EXPDIR/groundtruth/$DATASET/pages_test.mlf" ) \
             <( gzip -dc "$f" | "$htrsh_diplomatizer" ) \
           > "$DDIR/evaluate_${wg}_diplomatic_wer.txt";
-        htrsh_mlf_to_tasas \
+        htrsh_prep_tasas \
             <( "$htrsh_diplomatizer" < "$EXPDIR/groundtruth/$DATASET/pages_test.mlf" ) \
             <( gzip -dc "$f" | "$htrsh_diplomatizer" ) \
             -c yes \
@@ -656,93 +656,4 @@ htrsh_randsplit () {
           print > outfile;
         }
         END { printf( "%s\n", part >= 0 ? part+1 : "" ); }';
-}
-
-##
-## Function that transforms two MLF files to the format used by tasas
-##
-htrsh_mlf_to_tasas () {
-  local FN="htrsh_mlf_to_tasas";
-  local SEPCHARS="no";
-  if [ $# -lt 2 ]; then
-    { echo "$FN: Error: Not enough input arguments";
-      echo "Description: Transforms two MLF files to the format used by tasas";
-      echo "Usage: $FN MLF_LAB MLF_REC [ Options ]";
-      echo "Options:";
-      echo " -c (yes|no)  Whether to separate characters for CER computation (def.=$SEPCHARS)";
-    } 1>&2;
-    return 1;
-  fi
-
-  ### Parse input arguments ###
-  local MLF_LAB="$1";
-  local MLF_REC="$2";
-  shift 2;
-  while [ $# -gt 0 ]; do
-    if [ "$1" = "-c" ]; then
-      SEPCHARS="$2";
-    else
-      echo "$FN: error: unexpected input argument: $1" 1>&2;
-      return 1;
-    fi
-    shift 2;
-  done
-
-  if ! [ -e "$MLF_LAB" ]; then
-    echo "$FN: error: MLF file not found: $MLF_LAB" 1>&2;
-    return 1;
-  elif ! [ -e "$MLF_REC" ]; then
-    echo "$FN: error: MLF file not found: $MLF_REC" 1>&2;
-    return 1;
-  fi
-
-  local GAWK_FORMAT_MLF='
-    BEGIN {
-      NMLF = 0;
-      SEPCHARS = SEPCHARS == "yes" ? 1 : 0 ;
-    }
-    { if( match( $1, /^"\*\// ) ) {
-        $1 = gensub( /^"\*\/(.+)\.[^.]+"$/, "\\1", "", $1 );
-        printf( NMLF == 0 ? "%s\t" : "\n%s\t", $1 );
-        NMLF ++;
-        NTOKENS = 0;
-      }
-      else if( $0 != "#!MLF!#" && $0 != "." ) {
-        if( NF >= 3 )
-          $1 = $3;
-        if( match( $1, /^".+"$/ ) )
-          $1 = gensub( /\\"/, "\"", "g", gensub( /^"(.+)"$/, "\\1", "", $1 ) );
-        if( ! SEPCHARS ) {
-          printf( NTOKENS == 0 ? "%s" : " %s", $1 );
-          NTOKENS ++;
-        }
-        else {
-          if( NTOKENS > 0 )
-            printf( " @" );
-          N = split( $1, chars, "" );
-          for( n=1; n<=N; n++ ) {
-            printf( NTOKENS == 0 ? "%s" : " %s", chars[n] );
-            NTOKENS ++;
-          }
-        }
-      }
-    }
-    END {
-      if( NMLF > 0 )
-        printf( "\n" );
-    }';
-
-  awk -v SEPCHARS=$SEPCHARS "$GAWK_FORMAT_MLF" "$MLF_LAB" \
-    | awk -F'\t' '
-        { if( FILENAME == "-" )
-            LAB[$1] = $2;
-          else {
-            if( !( $1 in LAB ) )
-              printf( "warning: no lab for %s\n", $1 ) > "/dev/stderr";
-            else
-              printf( "%s|%s\n", LAB[$1], $2 );
-          }
-        }' - <( awk -v SEPCHARS=$SEPCHARS "$GAWK_FORMAT_MLF" "$MLF_REC" );
-
-  return 0;
 }
